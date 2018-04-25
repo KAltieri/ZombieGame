0e5c7f3c5d2ccd55ef5880d1891ec369d14429da
Kevin <kdalt412@gmail.com>
2018-04-25 12:04:00 -0400

    Kevin - April 25th 12:00
    Cover Manager & covers

---
+++ ZombieGame/Assets/Scripts/System/CoverManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverManager : MonoBehaviour {
+
+	private GameObject player;
+	private int count = -1;
+	[SerializeField] private float reduction = .1f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(count > 0)
+		{
+			Debug.Log (transform.GetChild (count));
+			Color temp = transform.GetChild (count).GetComponent<SpriteRenderer> ().color;
+			temp.a -= reduction;
+			transform.GetChild (count).GetComponent<SpriteRenderer> ().color = temp;
+		}
+		count = LevelManager.instance.getLevelNum (player.transform.position.y);
+		if (Time.timeSinceLevelLoad < 1f) 
+		{
+			count = 0;
+		}
+	}
+}
